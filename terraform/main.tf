a00e1f263c32e8c7d46c60c35f5874d1d2911a41
GIORGI SHIOSHVILI <gioshio85@gmail.com>
2023-04-28 17:53:01 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,4 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "drafiedev@actright.com"
-    AccountName               = "Development"
-    ManagedOrganizationalUnit = "Environments"
-    SSOUserEmail              = "drafiedev@actright.com"
-    SSOUserFirstName          = "drafie"
-    SSOUserLastName           = "dev"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-    "Env" = "Dev"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
+################################# DEV ##################################
   
   module "dev" {
   source = "./modules/aft-account-request"
